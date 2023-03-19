cd96f7f70bc43e9cb4f0b374a7b3fc7d839ddd91
parallel-dev-ecm <emilio.cortes@parallelroom.io>
2023-03-19 15:18:05 -0600

    Styling

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { FIREBASE_OBJECTS } from "../firebase";
 import Navbar from "./Navbar.jsx";
 import Slider from "./UI/Slider";
+// @ts-ignore
+import ToggleField from "./UI/ToggleField";
 
 type Props = {};
 
@@ -16,13 +18,24 @@ function Dashboard({}: Props) {
       <br />
       <div className="dashBoard">
         <div className=" min-h-screen ">
-          <div className="firstSliderRow container lg:flex mx-auto px-4  sm:px-6 lg:px-8  ">
+          <div className="firstSliderRow container md:flex  mx-auto px-4  sm:px-6 lg:px-8  ">
             <Slider collectionField="value" />
             <Slider collectionField="test1" />
             <Slider collectionField="test2" />
+
+            <div className="sliderDiv  border-r"></div>
             <Slider collectionField="test1" />
             <Slider collectionField="value" />
             <Slider collectionField="test2" />
+          </div>
+          <br />
+          <br />
+          <div className="toggleContainer border  p-6  container mx-auto w-2/4 ">
+            <ToggleField />
+            <br />
+            <ToggleField />
+            <br />
+            <ToggleField />
           </div>
         </div>
       </div>
